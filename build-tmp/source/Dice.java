0f266466872cac18052142d818ecdf88f25ea983
unknown <nirolph@697-334-Nogrod.sf.k12.internal>
2014-09-12 10:00:09 -0700

    Two Dice

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,12 +1,28 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die one;
 Die two;
-void setup()
+public void setup()
 {
     size(500,500);
     noLoop();
     background(255, 0, 180);
 }
-void draw()
+public void draw()
 {
     one = new Die (190,225);
     two = new Die(260,225);
@@ -15,7 +31,7 @@ void draw()
     two.showCube();
     two.roll();
    }
-void mousePressed()
+public void mousePressed()
 {
     redraw();
 }
@@ -28,7 +44,7 @@ class Die //models one single dice cube
         DieY = y;
         DieNumber = (int)(Math.random()*6) + 1;
     }
-    void roll()
+    public void roll()
     {
         fill(0);
         strokeWeight(1);
@@ -85,10 +101,19 @@ class Die //models one single dice cube
         ellipse(DieX +35 , DieY + 40, 10, 10);
        }
     }
-    void showCube()
+    public void showCube()
     {
         fill(255);
-        strokeWeight(2.5);
+        strokeWeight(2.5f);
         rect(DieX, DieY, 50, 50);
     }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
